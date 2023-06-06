c4b4e19f1d6ad5e2f80f4d66bac0e32ac5e4601f
Poojashi <135614036+Poojashi@users.noreply.github.com>
2023-06-06 11:04:02 +0530

    Add files via upload
    Problem_no_1

---
+++ add_two_Float_numbers.java
@@ -0,0 +1,22 @@
+package assignment_1;
+
+public class add_two_Float_numbers {
+
+
+        public static void main (String[] args) throws Exception
+        {
+
+            Float a= 8.6f;
+            Float b= 9.7f;
+            Float c ;
+
+            c=a +b;
+            System.out.println("the sum of two floating numbers a and b is c-->" +c);
+
+
+        }
+
+
+
+
+}
