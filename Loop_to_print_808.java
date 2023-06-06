707596ac6f2919cb2555f65d3510d3b4b4ed3a40
Poojashi <135614036+Poojashi@users.noreply.github.com>
2023-06-06 12:22:22 +0530

    Add files via upload
    Problem_no_5

---
+++ Loop_to_print_808.java
@@ -0,0 +1,36 @@
+package assignment_5;
+
+public class Loop_to_print_808 {
+
+    public static void main (String[] args) throws Exception
+    {
+
+        Loop_to_print_808 obj= new Loop_to_print_808();
+
+        obj.runaforloop_1_to_100(1000);
+
+
+
+
+
+
+
+    }
+
+
+    public void runaforloop_1_to_100(int x)
+    {
+
+
+
+        for (int i=500;i<=x;i++)
+        {
+
+            if (i==808)
+            {
+                System.out.println("The number found as " +i);
+            }
+
+        }
+    }
+}
