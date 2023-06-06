ff76c270634fd0a64fcc8e364733715c4b77c5fc
Poojashi <135614036+Poojashi@users.noreply.github.com>
2023-06-06 12:27:20 +0530

    Add files via upload
    Problem_no_9

---
+++ Sustring.java
@@ -0,0 +1,16 @@
+package assignment_9;
+
+public class Sustring {
+
+
+        public static void main(String args[])
+        {
+
+            String Str = new String("ABCDE12345");
+
+
+            System.out.print("The extracted substring is : ");
+            System.out.println(Str.substring(5));
+        }
+    }
+
